76a9f91753bfffb34ae24649f66d68dc02c0d820
sangdon <ssf1103@nvaver.com>
2023-06-15 16:02:08 +0900

    dfdsf

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone_test/screen/Home_screen.dart';
+import 'package:netflix_clone_test/screen/search_screen.dart';
+import 'screen/more_screen.dart';
+import 'widget/bottom_bar.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BbongFlix',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.blue, brightness: Brightness.dark)
+            .copyWith(secondary: Colors.black),
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              HomeScreen(),
+              SearchScreen(),
+              Container(
+                child: Center(
+                  child: Text('save'),
+                ),
+              ),
+              MoreScreen(),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+}
